e61148f7210ef0e0377cd2eae1a987008b2a93ee
faryna <slavik.faryna@gmail.com>
2015-10-01 01:55:27 +0300

    explorer

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace ExplorerExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.AddressString = new System.Windows.Forms.ToolStripTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder.png");
+            this.imageList1.Images.SetKeyName(1, "document.png");
+            this.imageList1.Images.SetKeyName(2, "default.jpeg");
+            this.imageList1.Images.SetKeyName(3, "FolderListView.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "document.png");
+            this.imageList2.Images.SetKeyName(1, "default.jpeg");
+            this.imageList2.Images.SetKeyName(2, "FolderListView.png");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(258, 31);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(506, 388);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listView1_ItemDrag);
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            this.columnHeader2.Width = 90;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Modified";
+            this.columnHeader3.Width = 157;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.ItemHeight = 16;
+            this.treeView1.Location = new System.Drawing.Point(3, 31);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 1;
+            this.treeView1.Size = new System.Drawing.Size(249, 388);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.66667F));
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.toolStrip2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 208F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(767, 630);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(258, 425);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(506, 202);
+            this.listBox1.TabIndex = 4;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(255, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::ExplorerExam.Resource1.prev;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::ExplorerExam.Resource1.next;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddressString});
+            this.toolStrip2.Location = new System.Drawing.Point(255, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(512, 25);
+            this.toolStrip2.TabIndex = 7;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // AddressString
+            // 
+            this.AddressString.Name = "AddressString";
+            this.AddressString.Size = new System.Drawing.Size(300, 25);
+            this.AddressString.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AddressString_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 630);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripTextBox AddressString;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+namespace ExplorerExam
+{
+    public partial class Form1 : Form
+    {
+        private void threadProc()
+        {
+            int ic = 0;
+            for ( ; ; )
+            {
+                if (ic != count)
+                {
+                    listBox1.Items.Clear();
+                    foreach (string str in selectFolder)
+                    {
+                        listBox1.Items.Add(str);
+                    }
+                    ic = count;
+                }
+                label1.Text = "point Path = " + PointPath.ToString();
+            }
+        }
+        private List<string> selectFolder;
+        private string[] strRoot;
+        private static int PointPath = 0;
+        private static int count = 0;
+        private bool poinNULL = true;
+        Thread t;
+        public Form1()
+        {
+            InitializeComponent();
+            imageList2.Images.Add(SystemIcons.WinLogo);
+            t = new Thread(new ThreadStart(threadProc));
+            t.Start();
+            selectFolder = new List<string>();
+            selectFolder.Add("root");
+            strRoot = Directory.GetLogicalDrives();
+            int size = strRoot.Length;
+
+            for (int i = 0; i < size; i++)
+            {
+
+                try
+                {
+                    treeView1.Nodes.Add(strRoot[i]);
+                    treeView1.Nodes[treeView1.Nodes.Count - 1].ImageIndex = 2;
+                    DirectoryInfo info = new DirectoryInfo(strRoot[i] + "/");
+                    DirectoryInfo[] sd = info.GetDirectories();
+                    foreach (DirectoryInfo inf in sd)
+                    {
+
+                        treeView1.Nodes[i].Nodes.Add(inf.Name);
+                        treeView1.Nodes[i].Nodes[treeView1.Nodes[i].Nodes.Count - 1].Tag = inf.FullName;
+                        try
+                        {
+                            if (new DirectoryInfo(inf.FullName).GetDirectories()[0] != null)
+                            {
+                                treeView1.Nodes[i].Nodes[treeView1.Nodes[i].Nodes.Count - 1].Nodes.Add("temp");
+                                treeView1.Nodes[i].Nodes[treeView1.Nodes[i].Nodes.Count - 1].Nodes[treeView1.Nodes[i].Nodes[treeView1.Nodes[i].Nodes.Count - 1].Nodes.Count - 1].Tag = "temp";
+                            }
+                        }
+                        catch (Exception e)
+                        {
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                ViewRootDir();
+            }
+        }
+        private void ViewRootDir()
+        {
+            listView1.Items.Clear();
+            int size = strRoot.Length;
+            for (int i = 0; i < size; i++)
+            {
+                try
+                {
+                    listView1.Items.Add(strRoot[i]);
+                    listView1.Items[listView1.Items.Count - 1].ImageIndex = 1;
+                    listView1.Items[listView1.Items.Count - 1].Tag = strRoot[i];
+
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            AddressString.Text = selectFolder[0];
+        }
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes[0].Tag == "temp")
+            {
+                e.Node.Nodes[0].Remove();
+                DirectoryInfo info = new DirectoryInfo(e.Node.Tag.ToString());
+                DirectoryInfo[] sd = info.GetDirectories();
+
+                foreach (DirectoryInfo inf in sd)
+                {
+                    e.Node.Nodes.Add(inf.Name);
+                    e.Node.Nodes[e.Node.Nodes.Count - 1].Tag = inf.FullName;
+                    try
+                    {
+                        if (new DirectoryInfo(inf.FullName).GetDirectories()[0] != null)
+                        {
+                            e.Node.Nodes[e.Node.Nodes.Count - 1].Nodes.Add("ppp");
+                            e.Node.Nodes[e.Node.Nodes.Count - 1].Nodes[e.Node.Nodes[e.Node.Nodes.Count - 1].Nodes.Count - 1].Tag = "temp";
+                        }
+                    }
+                    catch (Exception e1)
+                    {
+
+                    }
+                }
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            treeView1.SelectedImageIndex = treeView1.SelectedNode.ImageIndex;
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                ListView lv = sender as ListView;
+                string str = lv.SelectedItems[0].Tag.ToString();
+                ShowDir(str);
+                if (PointPath == 1)
+                {
+                    selectFolder.Clear();
+                    selectFolder.Add("root");
+                    count = 1;
+                    PointPath = 0;
+                }
+                selectFolder.Add(str);
+                PointPath = selectFolder.Count;
+                count++;
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    ListView lv = sender as ListView;
+                    string str = lv.SelectedItems[0].Text;
+                    System.Diagnostics.Process.Start(selectFolder[PointPath-1] + str);
+                }
+                catch (Exception e1)
+                { }
+            }
+        }
+
+        private void ShowDir(string lv)
+        {
+            try
+            { 
+                DirectoryInfo info = new DirectoryInfo(lv);
+                FileInfo[] fileInfo = info.GetFiles();
+                DirectoryInfo[] inf = info.GetDirectories();
+                listView1.Items.Clear();
+                foreach (DirectoryInfo i in inf)
+                {
+                    listView1.Items.Add(i.Name);
+                    listView1.Items[listView1.Items.Count - 1].Tag = i.FullName;
+                    listView1.Items[listView1.Items.Count - 1].ImageIndex = 2;
+                }
+                foreach (FileInfo i in fileInfo)
+                {
+                    listView1.Items.Add(i.Name);
+                    imageList2.Images.Add(Icon.ExtractAssociatedIcon(i.FullName));
+                    listView1.Items[listView1.Items.Count - 1].ImageIndex = imageList2.Images.Count-1;
+                }
+                AddressString.Text = lv;
+                
+            }
+            catch (Exception e2)
+            {
+                throw;
+                //MessageBox.Show("Error");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            t.Abort();
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                ShowDir(e.Node.FullPath);
+                if (selectFolder[selectFolder.Count - 1] != e.Node.FullPath)
+                {
+                    selectFolder.Add(e.Node.FullPath);
+                    PointPath = selectFolder.Count;
+                    count++;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectFolder[PointPath - 2] != "root")
+                {
+                    ShowDir(selectFolder[PointPath - 2]);
+                    PointPath--;
+                }
+                else
+                {
+                    ViewRootDir();
+                    PointPath--;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (PointPath - 1 < count)
+                {
+                    ShowDir(selectFolder[PointPath]);
+                    PointPath++;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void AddressString_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    ShowDir(AddressString.Text);
+                    if (selectFolder[selectFolder.Count - 1] != AddressString.Text)
+                    {
+                        selectFolder.Add(AddressString.Text);
+                        PointPath = selectFolder.Count;
+                        count++;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void listView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            var listView = sender as ListView;
+
+            // Вызываем метод DoDragDrop чтобы начать перетаскивание.
+            // Первый аргумент - это то, что мы хотим перетащить. В данном случае это все выделенные элементы.
+            // Можно воспользоваться свойством e.Item, но тогда мы сможем перетаскивать только один элемент за раз.
+            // Второй агумент - это действие, которое мы позволяем делать с элементом. В данном случае - перенести на другой лист
+            listView.DoDragDrop(listView.SelectedItems, DragDropEffects.Move);
+        }
+
+        private void listView1_DragEnter(object sender, DragEventArgs e)
+        {
+            var listView = sender as ListView;
+
+            // Суть этого метода - поставить необходимое значение e.Effect. То есть какое действие должно выполниться после отпускания кнопки мыши:
+            //  копирование, перенесение и т.д.
+            // Значение этого параметра должно попадать в список разрешенных эффектов, установленных в методе DoDragDrop
+
+            // В данном случае мы проверяем чтобы элементы, которые перетаскиваются, принадлежали определенному типу (иначе можно перетаскивать все подряд: файлы, ссылки, другие элементы).
+            // То есть проверяем чтобы перетаскиваемые элементы были именно коллекцией выделенных элементов из контрола.
+            // Если проверка проходит и разрешенный эффект совпадает с тем, что мы хотим сделать, ставим значение e.Effect на нужное действие - то, которое выполнится при отпускании мыши.
+            if (e.Data.GetDataPresent("System.Windows.Forms.ListView+SelectedListViewItemCollection") && e.AllowedEffect == DragDropEffects.Move)
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void listView1_DragDrop(object sender, DragEventArgs e)
+        {
+            // Здесь выполняется непосредственное добавление/удаление элементов.
+            // Если обработчик события DragEnter не разрешил действие, то после отпускания мыши это событие не будет вызвано.
+            var listView = sender as ListView;
+
+            // Достаем перетаскиваемые данные и приводим их к нужному нам типу - коллекции выделенных элементов.
+            var items = e.Data.GetData("System.Windows.Forms.ListView+SelectedListViewItemCollection") as ListView.SelectedListViewItemCollection;
+
+            // Начинаем обработку каждого элемента в коллекции
+            foreach (ListViewItem item in items)
+            {
+                item.ListView.Items.Remove(item);	// Так как мы перетаскиваем из одного листа в другой, то элемент сначала надо удалить из "родительского" листа
+                listView.Items.Add(item);           // Добавляем элемент в лист, на который его сбросили
+            }
+        }
+        
+    }
+}
